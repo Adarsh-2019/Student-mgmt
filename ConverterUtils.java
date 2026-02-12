3ebd6a2df18e39d79bddbefae1b2e6e7156dab39
Java-developer <angad201917@gmail.com>
2026-02-12 12:06:11 +0530

    Update ConverterUtils.java

--- ConverterUtils.java
+++ ConverterUtils.java
@@ -1,1 +1,24 @@
 
+package com.lkm.utils;
+
+import com.lkm.entity.StudentDTO;
+import com.lkm.entity.StudentEntity;
+import org.springframework.beans.BeanUtils;
+
+public class ConverterUtils {
+
+    //convert dto to entity
+    public static StudentEntity convertDTOToEntity(StudentDTO dto){
+        StudentEntity entity=new StudentEntity();
+        BeanUtils.copyProperties(dto,entity);
+        return entity;
+    }
+
+    //convert entity to dto
+    public static StudentDTO convertEntityToDTO(StudentEntity entity){
+        StudentDTO dto=new StudentDTO();
+        BeanUtils.copyProperties(entity,dto);
+        return dto;
+    }
+
+}
